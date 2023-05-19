2326cb671654c33f547ccefeb88edb733968a618
Rigotka <sergey_sorokin.1580@mail.ru>
2023-05-20 03:09:25 +0700

    add content lab2.php

--- labs/lab2/lab2.php
+++ labs/lab2/lab2.php
@@ -6,5 +6,27 @@
 </head>
 <body>
 	<h1>lab2 <a href="/index.php">вернуться</a></h1>
-</body>
+		<div class = description>
+			Целью данной работы является знакомство с циклами в PHP. 
+		</div>
+
+		<div class="items">
+			<div class="item">
+				<div class="item-name">Задание 1</div> 
+				<div class="item-value"><a href="tasks/task1/info.php">Информация о разработчике</a></div>
+			</div>
+			<div class="item">
+				<div class="item-name">Задание 2</div> 
+				<div class="item-value"><a href="tasks/task2/colors.php">Цвета HTML</a></div>
+			</div>
+			<div class="item">
+				<div class="item-name">Задание 3</div> 
+				<div class="item-value"><a href="tasks/task3/multiplication.php">Таблица умножения</a></div>
+			</div>
+			<div class="item">
+				<div class="item-name">Дата выполнения</div> 
+				<div class="item-value">20.05.2023</div>
+			</div>
+		</div>
+	</body>
 </html>
---
+++ labs/lab2/tasks/task2/colors.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Цвета HTML</title>
+</head>
+<body>
+	цвета 
+</body>
+</html>
