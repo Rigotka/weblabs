2ff907df6337308da4f109237dcaa29cfe31b1f0
Rigotka <sergey_sorokin.1580@mail.ru>
2023-05-20 03:36:11 +0700

    add content info.php

--- labs/lab2/tasks/task1/info.php
+++ labs/lab2/tasks/task1/info.php
@@ -1,11 +1,31 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Инфрмация о разработчике</title>
+	<title>Инфорамция о разработчике</title>
+	<link href="/styles/style.css" rel="stylesheet" type="text/css">
 </head>
 <body>
-	инфо
-</body>
+	<h1>Инфорамция о разработчике <a href="/labs/lab2/lab2.php">вернуться</a></h1>
+		<div class = description>
+			Заданеи выполнено с помощью массива и цикла foreach.
+		</div>
+
+		<div class="items">
+			<?php
+				$array = [
+					'ФИО' => 'Сорокин Сергей Александрович', 
+					"Группа" => "571-2",
+					"Возраст" => "21",
+					"Город" => "Томск",
+				];
+				foreach ($array as $key => $value) 
+				{
+					echo "<div class='item'>
+						  	<div class='item-name'>$key</div> 
+						  	<div class='item-value'>$value</div>
+					      </div>";
+				}
+			?>
+		</div>
+	</body>
 </html>
