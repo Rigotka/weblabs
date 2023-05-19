54ae7c3f7b2207794bd01260a896eb62ce10956a
Rigotka <sergey_sorokin.1580@mail.ru>
2023-05-20 06:05:53 +0700

    add content multiplication.php

--- labs/lab2/tasks/task3/multiplication.php
+++ labs/lab2/tasks/task3/multiplication.php
@@ -1,11 +1,32 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Таблица</title>
+	<title>Таблица умножения</title>
+	<link href="/styles/style.css" rel="stylesheet" type="text/css">
+	<link href="/styles/multiplication.css" rel="stylesheet" type="text/css">
 </head>
 <body>
-	умножения
-</body>
+	<h1>Таблица умножения <a href="/labs/lab2/lab2.php">вернуться</a></h1>
+		<div class = description>
+			Задании выполнено с помощью двух циклов for.
+		</div>
+
+		<div class="items">
+			<?php
+				for($i = 2; $i < 10; $i++)
+				{
+					echo "<div class='item'>";
+/*					echo "<div class='item-name'>$i</div>";
+*/					for($j = 1; $j < 10; $j++)
+					{
+						$result = $i.' * '.$j.' = '.($i * $j);
+/*						echo $result;
+*/						echo "<div class='item-value'>$result</div>";
+/*						echo "<br>";
+*/					}
+					echo "</div>";
+				}
+			?>
+		</div>
+	</body>
 </html>
