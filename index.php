2b152f62981068fc545740bbbf7f2491da438454
Rigotka <sergey_sorokin.1580@mail.ru>
2023-05-20 01:18:32 +0700

    add content index.php and styles

--- index.php
+++ index.php
@@ -1,11 +1,30 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Главная</title>
+	<link href="styles/style.css" rel="stylesheet" type="text/css">
 </head>
 <body>
-	начало
+	<h1>Сорокин Сергей</h1>
+		<div class = description>
+			На этой странице представлен проект, созданный в процессе изучения веб-технологий с использованием OpenServer. 
+			С данной страницы можно перейте на страницы лабораторных работ и Github, 
+			где доступна структура проекта и его исходный код.
+		</div>
+
+		<div class="items">
+			<div class="item">
+				<div class="item-name">lab1</div> 
+				<div class="item-value"><a href="">*клик*</a></div>
+			</div>
+			<div class="item">
+				<div class="item-name">lab2</div>
+				<div class="item-value"><a href="">*клик*</a></div>
+			</div>
+			<div class="item">
+				<div class="item-name">GitHub</div>
+				<div class="item-value"><a href="https://github.com/Rigotka/weblabs" target="_blank">*клик*</a></div>
+			</div>
+		</div>
 </body>
 </html>
